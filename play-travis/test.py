fe95779275949dd0aa658541f2e313ebfc216d32
Andrew <andrew.1.kulikov@gmail.com>
2021-12-15 23:09:41 +0300

    Add division test

--- play-travis/test.py
+++ play-travis/test.py
@@ -6,6 +6,9 @@ class NumbersTest(unittest.TestCase):
     def test_multiply(self):
         self.assertEqual(2 * 2, 4)
 
+    def test_divide(self):
+        self.assertEqual(7 // 2, 3)
+
     def test_equal(self):
         self.assertEqual(1 + 1, 1)
 
