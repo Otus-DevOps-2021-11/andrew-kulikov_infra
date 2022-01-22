d00e40a8253851115694c26c415da69c169c7751
Andrew <andrew.1.kulikov@gmail.com>
2022-01-22 13:46:54 +0300

    HW-6 Add load balacner ip address to outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,10 @@
 output "external_ip_address_app" {
   value = yandex_compute_instance.app.*.network_interface.0.nat_ip_address
 }
+
+output "external_ip_address_lb" {
+  value = [
+    for l in yandex_lb_network_load_balancer.reddit-load-balancer.listener :
+    [for spec in l.external_address_spec : spec.address]
+  ]
+}
