427c63b7a63535710cdbf8b1f4658f261d128c02
Andrew <andrew.1.kulikov@gmail.com>
2022-01-22 13:25:36 +0300

    HW-6 Add terraform app with loadbalancer configurations

---
+++ terraform/lb.tf
@@ -0,0 +1,38 @@
+resource "yandex_lb_target_group" "reddit-target-group" {
+  name = "reddit-target-group"
+
+  dynamic "target" {
+    for_each = yandex_compute_instance.app
+    content {
+      subnet_id = var.subnet_id
+      address   = target.value.network_interface.0.ip_address
+    }
+  }
+}
+
+resource "yandex_lb_network_load_balancer" "reddit-load-balancer" {
+  name = "reddit-load-balancer"
+
+  type = "external"
+
+  listener {
+    name        = "reddit-app-listener"
+    port        = 80
+    target_port = 9292
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+
+  attached_target_group {
+    target_group_id = yandex_lb_target_group.reddit-target-group.id
+
+    healthcheck {
+      name = "http"
+      http_options {
+        port = 9292
+        path = "/"
+      }
+    }
+  }
+}
