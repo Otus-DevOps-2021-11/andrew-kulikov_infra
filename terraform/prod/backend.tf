45d1161b08641bed2f62ad2f124b60e2832db131
Andrew <andrew.1.kulikov@gmail.com>
2022-01-25 23:21:39 +0300

    HW-7 Format terraform files

--- terraform/prod/backend.tf
+++ terraform/prod/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    endpoint   = "storage.yandexcloud.net"
-    region     = "us-east-1"
-    key        = "terraform.tfstate"
+    endpoint = "storage.yandexcloud.net"
+    region   = "us-east-1"
+    key      = "terraform.tfstate"
 
     skip_region_validation      = true
     skip_credentials_validation = true
