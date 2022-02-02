99e1b8b3bccc3ebe03bf4c9c60b0d65783e48c94
Andrew <andrew.1.kulikov@gmail.com>
2022-02-02 22:57:43 +0300

    HW-8 Add report

--- ansible/yc.py
+++ ansible/yc.py
@@ -34,7 +34,7 @@ def get_parsed_terraform_outputs():
     return parse_terraform_outputs(output_lines)
 
 
-class ExampleInventory(object):
+class YCTerraformInventory(object):
 
     def __init__(self):
         self.inventory = {}
@@ -90,4 +90,4 @@ class ExampleInventory(object):
         self.args = parser.parse_args()
 
 
-ExampleInventory()
+YCTerraformInventory()
